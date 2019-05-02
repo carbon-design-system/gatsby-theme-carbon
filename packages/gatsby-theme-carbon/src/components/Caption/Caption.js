adae1395057bf103a9bafd2aba77498b780b0a87
Alison Joseph <alisonejoseph@gmail.com>
2019-05-02 11:05:56 -0500

    add fullWidth prop to caption

--- packages/gatsby-theme-carbon/src/components/Caption/Caption.js
+++ packages/gatsby-theme-carbon/src/components/Caption/Caption.js
@@ -10,16 +10,22 @@ export default class Caption extends React.Component {
     children: PropTypes.node,
 
     /**
+     * Set to full width
+     */
+    fullWidth: PropTypes.bool,
+
+    /**
      * Specify a custom class
      */
     className: PropTypes.string,
   };
 
   render() {
-    const { children, className } = this.props;
+    const { children, className, fullWidth } = this.props;
 
     const captionClasses = classnames([`${prefix}--caption`], {
       [className]: className,
+      [`${prefix}--caption--full`]: fullWidth,
     });
 
     return <p className={captionClasses}>{children}</p>;
