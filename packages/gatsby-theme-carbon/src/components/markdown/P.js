9c9e9d7de741eb45662c5018749a0dc876728d90
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-05-01 20:04:14 -0500

    feat: remove markdown rows

--- packages/gatsby-theme-carbon/src/components/markdown/P.js
+++ packages/gatsby-theme-carbon/src/components/markdown/P.js
@@ -1,22 +1,22 @@
 import React from 'react';
+import responsiveStyles from '../shared/responsiveStyles';
 
-import Row from '../Row';
-
-const pCss = ({ typeStyles, layout }) => ({
-  ...typeStyles.bodyLong02,
-  marginBottom: layout[2],
-  '& :empty': {
-    padding: 0,
-    margin: 0,
+const pCss = ({ typeStyles, layout }) => [
+  typeStyles.bodyLong02,
+  responsiveStyles,
+  {
+    marginBottom: layout[1],
+    '& :empty': {
+      padding: 0,
+      margin: 0,
+    },
   },
-});
+];
 
 const P = ({ children }) => (
-  <Row type="p">
-    <p className="markdown-p" css={pCss}>
-      {children}
-    </p>
-  </Row>
+  <p css={pCss} className="carbon--mdx__p">
+    {children}
+  </p>
 );
 
 export default P;
