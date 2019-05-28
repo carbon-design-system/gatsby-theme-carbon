5df549c83abd6d3a0df838c8d4d59ff5f5960ec3
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-05-28 16:15:13 -0500

    fix: update naming and margins

--- packages/gatsby-theme-carbon/src/components/FourOhFour/FourOhFour.js
+++ packages/gatsby-theme-carbon/src/components/FourOhFour/FourOhFour.js
@@ -10,35 +10,28 @@ import {
   list,
 } from './FourOhFour.module.scss';
 
-const whoops = ({ links }) => {
-  const getLinks = () => (
-    <>
-      {' '}
-      Maybe some of these most visited links will help you?
-      <ul className={list}>
-        {links.map(({ href, text }, i) => (
-          <li key={i}>
-            <Link className={link} to={href}>
-              {text}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+const FourOhFour = ({ links }) => (
+  <Layout homepage>
+    <div className={container}>
+      <h2 className={heading}>Something’s gone wrong...</h2>
+      <p className={paragraph}>
+        Sorry, we can’t find the page you are looking for.
+        {links && ' Maybe some of these most visited links will help you?'}
+      </p>
+      {links && (
+        <ul className={list}>
+          {links.map(({ href, text }, i) => (
+            <li key={i}>
+              <Link className={link} to={href}>
+                {text}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+      <span className={fourOhFour}>404</span>
+    </div>
+  </Layout>
+);
 
-  return (
-    <Layout homepage>
-      <div className={container}>
-        <h2 className={heading}>Something’s gone wrong...</h2>
-        <p className={paragraph}>
-          Sorry, we can’t find the page you are looking for.
-          {links && getLinks()}
-        </p>
-        <span className={fourOhFour}>404</span>
-      </div>
-    </Layout>
-  );
-};
-
-export default whoops;
+export default FourOhFour;
