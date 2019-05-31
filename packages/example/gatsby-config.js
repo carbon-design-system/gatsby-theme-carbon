e1cb0dd334853170ac9ff6c01f366121f2f47e09
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-05-31 10:50:52 -0500

    chore: add manifest override to example

--- packages/example/gatsby-config.js
+++ packages/example/gatsby-config.js
@@ -2,6 +2,20 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Theme Carbon',
   },
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Carbon Design Gatsby Theme',
+        short_name: 'Gatsby Theme Carbon',
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#0062ff',
+        display: 'browser',
+        icon: './src/images/favicon.png',
+      },
+    },
+  ],
   __experimentalThemes: [
     {
       resolve: 'gatsby-theme-carbon',
