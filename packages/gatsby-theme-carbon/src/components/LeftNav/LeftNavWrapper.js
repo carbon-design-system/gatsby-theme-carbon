69eabd0116af1651705f0fea51a45d26dde870c6
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-06-04 16:30:06 -0500

    fix: remove jank fix

--- packages/gatsby-theme-carbon/src/components/LeftNav/LeftNavWrapper.js
+++ packages/gatsby-theme-carbon/src/components/LeftNav/LeftNavWrapper.js
@@ -7,21 +7,12 @@ const LeftNavWrapper = styled.div(({ expanded, shouldHideHeader }) => ({
   position: 'fixed',
   top: shouldHideHeader ? '0px' : '48px',
   bottom: 0,
-  overflow: 'hidden',
+  overflow: 'auto',
+  scrollbarWidth: 'none',
   zIndex: 10000, // z('floating')
   transform: expanded ? 'translateX(0px)' : 'translateX(-256px)',
   '.bx--side-nav.bx--side-nav--website': {
     boxShadow: expanded ? '0 2px 8px rgba(0, 0, 0, 0.2)' : 'none',
-  },
-  '@media (max-width: 42rem)': {
-    'bx--side-nav__icon': {
-      display: 'inline-block !important;',
-    },
-    'nav > ul': {
-      maxHeight: '85vh',
-      overflowY: 'scroll',
-      scrollbarWidth: 'none',
-    },
   },
   [mq.lg]: {
     transform: 'translateX(0px)',
