1b55582bf857f21f4b155139e5436f30ee19ca82
Vince Picone <vpicone@gmail.com>
2019-06-04 16:45:02 -0500

    fix: use more intelligent active state inference for sidenav (#144)
    * fix: prevent nav jank and active state
    
    * fix: scroll
    
    * fix: remove jank fix

--- packages/gatsby-theme-carbon/src/components/LeftNav/LeftNavItem.js
+++ packages/gatsby-theme-carbon/src/components/LeftNav/LeftNavItem.js
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react';
 import { Link } from 'gatsby';
 import { Location } from '@reach/router';
-import slugify from 'slugify';
 import cx from 'classnames';
 import {
   SideNavLink,
@@ -17,69 +16,65 @@ const LeftNavItem = props => {
   const { items, category } = props;
   const { toggleNavState } = useContext(NavContext);
   const closeLeftNav = () => toggleNavState('leftNavIsOpen', 'close');
-  if (items.length === 1) {
-    const isActive =
-      typeof window !== 'undefined' &&
-      window.location.pathname.includes(slugify(category, { lower: true }));
-    return (
-      <SideNavLink
-        onClick={closeLeftNav}
-        icon={<span>dummy icon</span>}
-        element={Link}
-        className={cx({ [currentItem]: isActive })}
-        isActive={isActive}
-        to={`${items[0].path}`}
-      >
-        <span className={cx({ [currentItemText]: isActive })}>{category}</span>
-      </SideNavLink>
-    );
-  }
 
   return (
     <Location>
-      {({ location }) => (
-        <SideNavMenu
-          icon={<span>dummy icon</span>}
-          isActive={location.pathname.includes(
-            slugify(category, { lower: true })
-          )} // TODO similar categories
-          defaultExpanded={location.pathname.includes(
-            slugify(category, { lower: true })
-          )}
-          title={category}
-        >
-          <SubNavItems
-            onClick={closeLeftNav}
-            items={items}
-            location={location}
-          />
-        </SideNavMenu>
-      )}
+      {({ location }) => {
+        const isActive = items.some(item =>
+          location.pathname.includes(item.path)
+        );
+        if (items.length === 1) {
+          return (
+            <SideNavLink
+              onClick={closeLeftNav}
+              icon={<span>dummy icon</span>}
+              element={Link}
+              className={cx({ [currentItem]: isActive })}
+              isActive={isActive}
+              to={`${items[0].path}`}
+            >
+              <span className={cx({ [currentItemText]: isActive })}>
+                {category}
+              </span>
+            </SideNavLink>
+          );
+        }
+        return (
+          <SideNavMenu
+            icon={<span>dummy icon</span>}
+            isActive={isActive} // TODO similar categories
+            defaultExpanded={isActive}
+            title={category}
+          >
+            <SubNavItems
+              onClick={closeLeftNav}
+              items={items}
+              location={location}
+            />
+          </SideNavMenu>
+        );
+      }}
     </Location>
   );
 };
 
-const SubNavItems = ({ items, location, onClick }) => {
-  const isActive = item => {
-    const titleSlug = slugify(item.title, { lower: true });
-    return location.pathname.includes(titleSlug);
-  };
-  return items.map((item, i) => {
-    const active = isActive(item);
-    return (
-      <SideNavMenuItem
-        to={`${item.path}`}
-        onClick={onClick}
-        element={Link}
-        isActive={active}
-        key={i}
+const SubNavItems = ({ items, location, onClick }) =>
+  items.map((item, i) => (
+    <SideNavMenuItem
+      to={`${item.path}`}
+      onClick={onClick}
+      element={Link}
+      isActive={location.pathname.includes(item.path)}
+      key={i}
+    >
+      <span
+        style={{
+          color: location.pathname.includes(item.path) ? '#171717' : 'inherit',
+        }}
       >
-        <span style={{ color: active ? '#171717' : 'inherit' }}>
-          {item.title}
-        </span>
-      </SideNavMenuItem>
-    );
-  });
-};
+        {item.title}
+      </span>
+    </SideNavMenuItem>
+  ));
 
 export default LeftNavItem;
