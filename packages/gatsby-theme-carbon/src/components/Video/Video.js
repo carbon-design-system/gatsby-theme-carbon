d3f58cc9fd8a4c4cebb3a7bb152deeed648a36ca
Alison Joseph <alisonejoseph@gmail.com>
2019-06-11 18:22:57 -0500

    feat(video): add locally sourced video support

--- packages/gatsby-theme-carbon/src/components/Video/Video.js
+++ packages/gatsby-theme-carbon/src/components/Video/Video.js
@@ -6,29 +6,50 @@ const { prefix } = settings;
 
 export default class Vimeo extends React.Component {
   static propTypes = {
-    vimeoId: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
+    vimeoId: PropTypes.string,
+    src: PropTypes.string,
+    title: PropTypes.string,
+    poster: PropTypes.string,
+  };
+
+  static defaultProps = {
+    src: '',
+    vimeoId: '',
   };
 
   render() {
-    const { vimeoId, title } = this.props;
+    const { vimeoId, title, src, poster } = this.props;
+
+    const vimeoVid = (
+      <div className="gatsby-resp-iframe-wrapper">
+        <div className="embedVideo-container">
+          <iframe
+            title={title}
+            src={`https://player.vimeo.com/video/${vimeoId}`}
+            width="640"
+            height="360"
+            frameBorder="0"
+            webkitallowfullscreen="true"
+            mozallowfullscreen="true"
+            allowFullScreen
+          />
+        </div>
+      </div>
+    );
+
+    const videoVid = (
+      <video controls poster={poster}>
+        <source src={src} type="video/mp4" />
+      </video>
+    );
 
     return (
       <div className={`${prefix}--video`}>
-        <div className="gatsby-resp-iframe-wrapper">
-          <div className="embedVideo-container">
-            <iframe
-              title={title}
-              src={`https://player.vimeo.com/video/${vimeoId}`}
-              width="640"
-              height="360"
-              frameBorder="0"
-              webkitallowfullscreen="true"
-              mozallowfullscreen="true"
-              allowFullScreen
-            />
-          </div>
-        </div>
+        {src === '' && vimeoId === '' ? (
+          <p>Please add a vimeoId or src to your video component</p>
+        ) : null}
+        {src !== '' ? <>{videoVid}</> : null}
+        {vimeoId !== '' ? <>{vimeoVid}</> : null}
       </div>
     );
   }
