a0a2da9f510fc2aae8ffea2a6b7f3f470fd3e0d7
Alison Joseph <alisonejoseph@gmail.com>
2019-06-11 18:50:58 -0500

    chore: update video example

--- packages/gatsby-theme-carbon/src/components/Video/Video.js
+++ packages/gatsby-theme-carbon/src/components/Video/Video.js
@@ -4,53 +4,51 @@ import { settings } from 'carbon-components';
 
 const { prefix } = settings;
 
-export default class Vimeo extends React.Component {
-  static propTypes = {
-    vimeoId: PropTypes.string,
-    src: PropTypes.string,
-    title: PropTypes.string,
-    poster: PropTypes.string,
-  };
-
-  static defaultProps = {
-    src: '',
-    vimeoId: '',
-  };
-
-  render() {
-    const { vimeoId, title, src, poster } = this.props;
-
-    const vimeoVid = (
-      <div className="gatsby-resp-iframe-wrapper">
-        <div className="embedVideo-container">
-          <iframe
-            title={title}
-            src={`https://player.vimeo.com/video/${vimeoId}`}
-            width="640"
-            height="360"
-            frameBorder="0"
-            webkitallowfullscreen="true"
-            mozallowfullscreen="true"
-            allowFullScreen
-          />
-        </div>
+export const Video = ({ vimeoId, title, src, poster, ...props }) => {
+  const vimeoVid = (
+    <div className="gatsby-resp-iframe-wrapper">
+      <div className="embedVideo-container">
+        <iframe
+          title={title}
+          src={`https://player.vimeo.com/video/${vimeoId}`}
+          width="640"
+          height="360"
+          frameBorder="0"
+          webkitallowfullscreen="true"
+          mozallowfullscreen="true"
+          allowFullScreen
+        />
       </div>
-    );
+    </div>
+  );
 
-    const videoVid = (
-      <video controls poster={poster}>
-        <source src={src} type="video/mp4" />
-      </video>
-    );
+  const videoVid = (
+    <video controls poster={poster} {...props}>
+      <source src={src} type="video/mp4" />
+    </video>
+  );
 
-    return (
-      <div className={`${prefix}--video`}>
-        {src === '' && vimeoId === '' ? (
-          <p>Please add a vimeoId or src to your video component</p>
-        ) : null}
-        {src !== '' ? <>{videoVid}</> : null}
-        {vimeoId !== '' ? <>{vimeoVid}</> : null}
-      </div>
-    );
-  }
-}
+  return (
+    <div className={`${prefix}--video`}>
+      {src === '' && vimeoId === '' ? (
+        <p>Please add a vimeoId or src to your video component</p>
+      ) : null}
+      {src !== '' ? <>{videoVid}</> : null}
+      {vimeoId !== '' ? <>{vimeoVid}</> : null}
+    </div>
+  );
+};
+
+Video.defaultProps = {
+  src: '',
+  vimeoId: '',
+};
+
+Video.propTypes = {
+  vimeoId: PropTypes.string,
+  src: PropTypes.string,
+  title: PropTypes.string,
+  poster: PropTypes.string,
+};
+
+export default Video;
