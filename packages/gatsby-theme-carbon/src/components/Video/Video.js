e947edb8e4f8253c0114b64791b5fa600dae810d
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-06-13 12:42:46 -0500

    chore: Add prop-type validation, vtt support, and use css modules

--- packages/gatsby-theme-carbon/src/components/Video/Video.js
+++ packages/gatsby-theme-carbon/src/components/Video/Video.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { settings } from 'carbon-components';
+import cx from 'classnames';
 
-const { prefix } = settings;
+import { video, vimeo } from './Video.module.scss';
 
-export const Video = ({ vimeoId, title, src, poster, ...props }) => {
-  if (vimeoId !== '') {
+export const Video = ({ vimeoId, title, ...props }) => {
+  if (vimeoId) {
     return (
-      <div className={`${prefix}--video ${prefix}--video--vimeo`}>
+      <div className={cx(video, vimeo)}>
         <div className="embedVideo-container">
           <iframe
             title={title}
@@ -22,32 +22,42 @@ export const Video = ({ vimeoId, title, src, poster, ...props }) => {
         </div>
       </div>
     );
-  } else if (src !== '') {
-    return (
-      <video
-        className={`${prefix}--video`}
-        controls
-        poster={poster}
-        {...props}
-        src={src}
-        type="video/mp4"
-      />
-    );
-  } else {
-    return null;
   }
-};
 
-Video.defaultProps = {
-  src: '',
-  vimeoId: '',
+  return (
+    // eslint-disable-next-line jsx-a11y/media-has-caption
+    <video className={video} controls type="video/mp4" {...props} />
+  );
 };
 
 Video.propTypes = {
   vimeoId: PropTypes.string,
+  children: PropTypes.element,
   src: PropTypes.string,
   title: PropTypes.string,
   poster: PropTypes.string,
+  videoSourceValidator: props => {
+    if (!props.vimeoId && !props.src) {
+      return new Error(
+        `The Video component requires either a 'vimeoId' Or a 'src' prop`
+      );
+    }
+    if (props.vimeoId && props.src) {
+      return new Error(
+        `You can only specify one source for the Video component. Use either the 'vimeoId' OR the 'src' prop.`
+      );
+    }
+    if (props.vimeoId && props.poster) {
+      return new Error(
+        `You can't specify a poster for vimeo videos. You can control the poster image from the Vimeo control panel`
+      );
+    }
+    if (props.vimeoId && props.children) {
+      return new Error(
+        `You can't specify children/tracks for vimeo videos. You can control the captions from the Vimeo control panel`
+      );
+    }
+  },
 };
 
 export default Video;
