9020ba3e7f76fabaf7d8dc68b1d81162dbb80554
David Conner <rdconner@us.ibm.com>
2019-07-03 13:17:03 -0400

    fix(accordion): move styles into index.scss and deleted accordion module

--- packages/gatsby-theme-carbon/src/components/Accordion/Accordion.js
+++ packages/gatsby-theme-carbon/src/components/Accordion/Accordion.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import cx from 'classnames';
 import { Accordion as CarbonAccordion } from 'carbon-components-react';
-import { accordion } from './Accordion.module.scss';
 
 const Accordion = ({ className, ...rest }) => (
-  <CarbonAccordion {...rest} className={cx(className, accordion)} />
+  <CarbonAccordion {...rest} className={cx(className)} />
 );
 export default Accordion;
