263a7c399562f5267b74b884cb58cdd221b7151d
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-07-08 16:04:03 -0500

    chore: remove unused content, update links

--- packages/example/src/gatsby-theme-carbon/components/LeftNav/ResourceLinks.js
+++ packages/example/src/gatsby-theme-carbon/components/LeftNav/ResourceLinks.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ResourceLinks from 'gatsby-theme-carbon/src/components/LeftNav/ResourceLinks';
 
 const links = [
-  { title: 'Resources', href: '/resources' },
-  { title: 'Storybook', href: 'https://react.carbondesignsystem.com' },
+  {
+    title: 'Github',
+    href: 'https://github.com/carbon-design-system/gatsby-theme-carbon',
+  },
+  {
+    title: 'Carbon Design System',
+    href: 'https://react.carbondesignsystem.com',
+  },
 ];
 
 // shouldOpenNewTabs: true if outbound links should open in a new tab
