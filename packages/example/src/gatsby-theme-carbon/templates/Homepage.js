b09ff9cede2250cf1ca61b7785b4a84331c2fd4e
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-07-09 15:24:57 -0500

    fix: homepage typography updates, alt tag and asset updates

--- packages/example/src/gatsby-theme-carbon/templates/Homepage.js
+++ packages/example/src/gatsby-theme-carbon/templates/Homepage.js
@@ -1,33 +1,36 @@
 import React from 'react';
 import { HomepageBanner, HomepageCallout } from 'gatsby-theme-carbon';
-
-// Component to be shadowed
 import HomepageTemplate from 'gatsby-theme-carbon/src/templates/Homepage';
+import { calloutLink } from './Homepage.module.scss';
 
 import Carbon from '../../images/carbon.jpg';
 
-const FirstLeftText = () => <p>Callout #1</p>;
+const FirstLeftText = () => <p>Callout component</p>;
 
 const FirstRightText = () => (
   <p>
     This is a callout component. You can edit the contents by updating the
-    pre-shadowed homepage template. You can provide a color and backgroundColor
-    props to suit your theme.
-    <br />
-    <a href="https://github.com/carbon-design-system/gatsby-theme-carbon/blob/master/packages/example/src/gatsby-theme-carbon/templates/Homepage.js">
+    pre-shadowed homepage template. You can provide <code>color</code> and{' '}
+    <code>backgroundColor</code> props to suit your theme.
+    <a
+      className={calloutLink}
+      href="https://github.com/carbon-design-system/gatsby-theme-carbon/blob/master/packages/example/src/gatsby-theme-carbon/templates/Homepage.js"
+    >
       Homepage source →
     </a>
   </p>
 );
 
-const SecondLeftText = () => <p>Callout #2</p>;
+const SecondLeftText = () => <p>Callout component</p>;
 
 const SecondRightText = () => (
   <p>
     You can also not use these components at all by not providing the callout
     props to the template or writing your own template.
-    <br />
-    <a href="https://github.com/carbon-design-system/gatsby-theme-carbon/blob/master/packages/example/src/gatsby-theme-carbon/templates/Homepage.js">
+    <a
+      className={calloutLink}
+      href="https://github.com/carbon-design-system/gatsby-theme-carbon/blob/master/packages/example/src/gatsby-theme-carbon/templates/Homepage.js"
+    >
       Homepage source →
     </a>
   </p>
@@ -38,7 +41,12 @@ const BannerText = () => <h1>Banner component</h1>;
 const customProps = {
   Banner: <HomepageBanner renderText={BannerText} image={Carbon} />,
   FirstCallout: (
-    <HomepageCallout leftText={FirstLeftText} rightText={FirstRightText} />
+    <HomepageCallout
+      backgroundColor="#030303"
+      color="white"
+      leftText={FirstLeftText}
+      rightText={FirstRightText}
+    />
   ),
   SecondCallout: (
     <HomepageCallout
