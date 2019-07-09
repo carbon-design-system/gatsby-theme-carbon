94f199b670126528964b6f614c8292e7463edcdf
Abbey Hart <abbeyhrt@gmail.com>
2019-07-09 14:03:40 -0500

    chore: add check for nested list to  update class names

--- packages/gatsby-theme-carbon/src/components/markdown/Li.js
+++ packages/gatsby-theme-carbon/src/components/markdown/Li.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import cx from 'classnames';
 
 const Li = ({ children, ...rest }) => (
   <li className="bx--list__item" {...rest}>
-    {children}
+    {React.Children.map(children, child => {
+      if (child.props) {
+        if (
+          (child.props.originalType === 'ul' ||
+            child.props.originalType === 'ol') &&
+          child.props.parentName === 'li'
+        ) {
+          return React.cloneElement(child, {
+            className: cx(child.props.className, 'bx--list--nested'),
+          });
+        }
+        return child;
+      }
+      return child;
+    })}
   </li>
 );
 
