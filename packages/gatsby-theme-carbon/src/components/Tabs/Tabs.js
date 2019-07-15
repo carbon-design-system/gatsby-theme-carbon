eb7285bfce02d7e1c1e7ee24a99bbe4da0746b39
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-07-15 17:09:36 -0500

    chore: add comment for Tabs component

--- packages/gatsby-theme-carbon/src/components/Tabs/Tabs.js
+++ packages/gatsby-theme-carbon/src/components/Tabs/Tabs.js
@@ -8,15 +8,17 @@ import { tabContainer, tab, tabContents } from './Tabs.module.scss';
 
 export const Tab = props => <CarbonTab className={tab} {...props}></CarbonTab>;
 
+// Memoized for performance durring window resizing. The cloneElement implementation and
+// subsequent memoization are necessary due to the following issue:
+// https://github.com/carbon-design-system/carbon/issues/339
 export const Tabs = React.memo(function Tabs(props) {
   return (
     <CarbonTabs className={tabContainer} tabContentClassName={tabContents}>
-      {React.Children.map(props.children, child => {
-        console.log('running');
-        return React.cloneElement(child, {
+      {React.Children.map(props.children, child =>
+        React.cloneElement(child, {
           renderContent: TabContent,
-        });
-      })}
+        })
+      )}
     </CarbonTabs>
   );
 });
