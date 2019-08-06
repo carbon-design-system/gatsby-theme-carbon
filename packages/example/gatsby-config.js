5b3c0de3f9934ca862490140db77f56bf4e31dff
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-08-06 12:05:59 -0500

    chore: remove additional font-weights, query rebuild

--- packages/example/gatsby-config.js
+++ packages/example/gatsby-config.js
@@ -20,7 +20,6 @@ module.exports = {
     {
       resolve: 'gatsby-theme-carbon',
       options: {
-        additionalFontWeights: ['200', '200i'],
         repository: {
           baseUrl:
             'https://github.com/carbon-design-system/gatsby-theme-carbon',
