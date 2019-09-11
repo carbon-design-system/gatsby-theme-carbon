e8f5a94b75a6d70d78e4dfa95a1948de6c0e3711
Vince Picone <vpicone@gmail.com>
2019-09-11 14:10:23 -0500

    [Search]: fix poor search results (#414)

--- packages/gatsby-theme-carbon/config/lunr-options.js
+++ packages/gatsby-theme-carbon/config/lunr-options.js
@@ -1,29 +1,30 @@
-const getFrontmatter = (node, item) => {
-  if (node.context && node.context.frontmatter) {
-    return node.context.frontmatter[item];
-  }
-  return '';
+const removeStemmerPlugin = lunr => builder => {
+  builder.pipeline.remove(lunr.stemmer);
+  builder.searchPipeline.remove(lunr.stemmer);
 };
 
 module.exports = {
-  languages: [{ name: 'en' }],
+  languages: [
+    {
+      name: 'en',
+      filterNodes: node => node.context && node.context.MdxNode,
+      plugins: [removeStemmerPlugin],
+    },
+  ],
   fields: [
-    { name: 'title', store: true, attributes: { boost: 20 } },
-    { name: 'keywords', attributes: { boost: 5 } },
+    { name: 'title', store: true, attributes: { boost: 30 } },
+    { name: 'keywords' },
     { name: 'path', store: true },
     { name: 'description', store: true },
     { name: 'content' },
   ],
   resolvers: {
     SitePage: {
-      title: node => getFrontmatter(node, 'title'),
       path: node => node.path,
-      description: node => getFrontmatter(node, 'description'),
-      keywords: node => getFrontmatter(node, 'keywords'),
-      content: node =>
-        node.context && node.context.MdxNode
-          ? node.context.MdxNode.internal.content
-          : '',
+      title: node => node.context.frontmatter.title,
+      description: node => node.context.frontmatter.description,
+      keywords: node => node.context.frontmatter.keywords,
+      content: node => node.context.MdxNode.internal.content,
     },
   },
 };
