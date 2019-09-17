2977c91df770c58d0b48d9b2ac05b634515cc60a
Alison Joseph <alisonejoseph@gmail.com>
2019-09-17 12:31:22 -0500

    feat: add InlineNotification component

---
+++ packages/gatsby-theme-carbon/src/components/InlineNotification/InlineNotification.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { InlineNotification as CarbonInlineNotification } from 'carbon-components-react';
+import { Row, Column } from '../Grid';
+
+import { notification } from './InlineNotification.module.scss';
+
+export default class InlineNotification extends React.Component {
+  static propTypes = {
+    children: PropTypes.node,
+    className: PropTypes.string,
+    kind: PropTypes.oneOf(['error', 'info', 'success', 'warning']),
+  };
+
+  static defaultProps = {
+    kind: 'info',
+  };
+
+  render() {
+    const { children, className, kind } = this.props;
+
+    const notifcationClasses = classnames(notification, {
+      [className]: className,
+    });
+
+    return (
+      <Row>
+        <Column noGutterSm colLg={8}>
+          <CarbonInlineNotification
+            lowContrast
+            hideCloseButton
+            kind={kind}
+            title=""
+            subtitle={children}
+            className={notifcationClasses}
+          />
+        </Column>
+      </Row>
+    );
+  }
+}
