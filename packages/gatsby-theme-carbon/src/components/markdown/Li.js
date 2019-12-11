0a0fb0fe99f84cab5b8eda768bc6362dbe0a20c4
Vince Picone <vpicone@gmail.com>
2019-12-11 13:26:26 -0600

    fix: update carbon and patch list styles (#603)
    * fix: update carbon and patch list styles
    
    * fix: refactor list item, add nested prop to ul/ol components

--- packages/gatsby-theme-carbon/src/components/markdown/Li.js
+++ packages/gatsby-theme-carbon/src/components/markdown/Li.js
@@ -4,21 +4,7 @@ import { listItem } from './Markdown.module.scss';
 
 const Li = ({ children, ...rest }) => (
   <li className={cx('bx--list__item', listItem)} {...rest}>
-    {React.Children.map(children, child => {
-      if (child.props) {
-        if (
-          (child.props.originalType === 'ul' ||
-            child.props.originalType === 'ol') &&
-          child.props.parentName === 'li'
-        ) {
-          return React.cloneElement(child, {
-            className: cx(child.props.className, 'bx--list--nested'),
-          });
-        }
-        return child;
-      }
-      return child;
-    })}
+    {children}
   </li>
 );
 
--- packages/gatsby-theme-carbon/src/components/markdown/Ol.js
+++ packages/gatsby-theme-carbon/src/components/markdown/Ol.js
@@ -2,10 +2,15 @@ import React from 'react';
 import cx from 'classnames';
 import { list } from './Markdown.module.scss';
 
-const Ol = ({ children, ...rest }) => (
-  <ol className={cx('bx--list--ordered', list)} {...rest}>
-    {children}
-  </ol>
-);
+const Ol = ({ children, nested, ...rest }) => {
+  const className = cx('bx--list--ordered', list, {
+    'bx--list--nested': nested,
+  });
+  return (
+    <ol className={className} {...rest}>
+      {children}
+    </ol>
+  );
+};
 
 export default Ol;
