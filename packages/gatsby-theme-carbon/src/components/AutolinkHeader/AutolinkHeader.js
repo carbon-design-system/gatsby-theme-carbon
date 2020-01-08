a01e607684095437352d2f5526f1a8cffd6c9245
Josefina Mancilla <32556167+jnm2377@users.noreply.github.com>
2020-01-08 15:11:52 -0600

    fix(autolink-header): slugify bug replaces ampersand (#629)
    * v1.17.2
    
    * v1.17.3
    
    * fix: autolink header with ampersand
    
    * fix: autolink header id
    
    Co-authored-by: Vince Picone <vpicone@gmail.com>

--- packages/gatsby-theme-carbon/src/components/AutolinkHeader/AutolinkHeader.js
+++ packages/gatsby-theme-carbon/src/components/AutolinkHeader/AutolinkHeader.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link20 as Link } from '@carbon/icons-react';
 import cx from 'classnames';
+import slugify from 'slugify';
 
 import { header, anchor } from './AutolinkHeader.module.scss';
 
@@ -10,14 +11,16 @@ const Anchor = ({ id, string }) => (
   </a>
 );
 
-const AutolinkHeader = ({ is: Component, id, className, ...props }) => {
+const AutolinkHeader = ({ is: Component, className, ...props }) => {
   const string = React.Children.map(
     props.children,
     child => (child.props ? child.props.children : child) // handle bold/italic words
   ).join('');
 
+  const id = `${slugify(string, { lower: true })}`;
+
   return (
-    <Component id={id} className={cx(header, className)} {...props}>
+    <Component className={cx(header, className)} {...props} id={id}>
       <Anchor id={id} string={string} />
       {props.children}
     </Component>
