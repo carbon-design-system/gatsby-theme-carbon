98cf7d014cbe7a5475a125a9d05a2ddd8a87fea7
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2020-01-08 20:11:30 -0600

    feat: add better subsetting for variable fonts, try zeit for static api

--- scripts/subset.sh
+++ scripts/subset.sh
@@ -1,5 +1,20 @@
-LATIN1="U+0000,U+000D,U+0020-007E,U+00A0-00A3,U+00A4-00FF,U+0131,U+0152-0153,U+02C6,U+02DA,U+02DC,U+2013-2014,U+2018-201A,U+201C-201E,U+2020-2022,U+2026,U+2030,U+2039-203A,U+2044,U+2074,U+20AC,U+2122,U+2212,U+FB01-FB02"
+LATIN="U+0020-00FF"
+LATINEXT="U+0100-024F"
+GREEK="U+0370-03FF"
+CYR="U+0400-04FF"
+CYREXT="U+0500-052F"
 ARROWS="U2191-21c6"
 
-pyftsubset IBMPlexSans-Italic-VF.ttf --unicodes=$LATIN1 --flavor="woff2" --output-file=IBMPlexSans-Italic-VF.woff2;
-pyftsubset IBMPlexSans-VF.ttf --unicodes=$LATIN1,$ARROWS --flavor="woff2" --output-file=IBMPlexSans-VF.woff2;
+pyftsubset src/IBMPlexSans-Italic-VF.ttf --unicodes=$LATIN --flavor="woff2" --output-file=IBMPlexSansLatin-Italic-VF.woff2;
+pyftsubset src/IBMPlexSans-Italic-VF.ttf --unicodes=$LATINEXT --flavor="woff2" --output-file=IBMPlexSansLatinExt-Italic-VF.woff2;
+pyftsubset src/IBMPlexSans-Italic-VF.ttf --unicodes=$VIETNAMESE --flavor="woff2" --output-file=IBMPlexSansVietnamese-Italic-VF.woff2;
+pyftsubset src/IBMPlexSans-Italic-VF.ttf --unicodes=$GREEK --flavor="woff2" --output-file=IBMPlexSansGreek-Italic-VF.woff2;
+pyftsubset src/IBMPlexSans-Italic-VF.ttf --unicodes=$CYR --flavor="woff2" --output-file=IBMPlexSansCyr-Italic-VF.woff2;
+pyftsubset src/IBMPlexSans-Italic-VF.ttf --unicodes=$CYREXT --flavor="woff2" --output-file=IBMPlexSansCyrExt-Italic-VF.woff2;
+
+pyftsubset src/IBMPlexSans-VF.ttf --unicodes=$LATIN,$ARROWS --flavor="woff2" --output-file=IBMPlexSansLatin-VF.woff2;
+pyftsubset src/IBMPlexSans-VF.ttf --unicodes=$LATINEXT --flavor="woff2" --output-file=IBMPlexSansLatinExt-VF.woff2;
+pyftsubset src/IBMPlexSans-VF.ttf --unicodes=$VIETNAMESE --flavor="woff2" --output-file=IBMPlexSansVietnamese-VF.woff2;
+pyftsubset src/IBMPlexSans-VF.ttf --unicodes=$GREEK --flavor="woff2" --output-file=IBMPlexSansGreek-VF.woff2;
+pyftsubset src/IBMPlexSans-VF.ttf --unicodes=$CYR --flavor="woff2" --output-file=IBMPlexSansCyr-VF.woff2;
+pyftsubset src/IBMPlexSans-VF.ttf --unicodes=$CYREXT --flavor="woff2" --output-file=IBMPlexSansCyrExt-VF.woff2;
