61ed7ece2dbabaab53eddebb02863f3286db0fb4
Vince Picone <vpicone@gmail.com>
2020-01-08 18:54:32 -0600

    fix: don't use css transitions while the page is still hydrating… (#638)
    * fix: don't use css transitions while the page is still hydrating closes #637
    
    * fix: wiildcard selector
    
    * fix: closes #614
    
    * chore: add mobile preview to dev script

--- packages/gatsby-theme-carbon/gatsby-browser.js
+++ packages/gatsby-theme-carbon/gatsby-browser.js
@@ -2,6 +2,14 @@ import wrapRoot from './src/util/wrap-root-element';
 
 export const wrapRootElement = wrapRoot;
 
+export const onClientEntry = () => {
+  // allow transitions once page has fully rendered
+  const body = document.querySelector('body');
+  window.addEventListener('load', () => {
+    body.classList.remove('body--preload');
+  });
+};
+
 const getTargetOffset = hash => {
   const id = window.decodeURI(hash.replace(`#`, ``));
   if (id !== ``) {
