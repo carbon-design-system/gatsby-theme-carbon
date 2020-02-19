030c97d3df70437c2797c9c61c81f07f8f3c7f83
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2020-02-19 13:55:34 -0600

    fix: use grid for next-prev

--- packages/gatsby-theme-carbon/src/components/NextPrevious/NextPreviousStyles.js
+++ packages/gatsby-theme-carbon/src/components/NextPrevious/NextPreviousStyles.js
@@ -1,33 +1,38 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import cx from 'classnames';
+import { Row, Column, Grid } from '../Grid';
 
 import {
   wrapper,
   link,
   direction,
   name,
-  firstLink,
-  secondLink,
   linkContainer,
+  grid,
 } from './NextPrevious.module.scss';
 
 const NextPreviousContainer = ({ previousItem, nextItem }) => (
   <div className={wrapper}>
-    <div className={linkContainer}>
-      {previousItem.to && (
-        <Link className={cx(link, firstLink)} to={previousItem.to}>
-          <div className={direction}>Previous</div>
-          <div className={name}>{previousItem.name}</div>
-        </Link>
-      )}
-      {nextItem.to && (
-        <Link className={cx(link, secondLink)} to={nextItem.to}>
-          <div className={direction}>Next</div>
-          <div className={name}>{nextItem.name}</div>
-        </Link>
-      )}
-    </div>
+    <Grid className={grid}>
+      <Row className={linkContainer}>
+        {previousItem.to && (
+          <Column className={link} colLg={6} colMd={4} colSm={2}>
+            <Link to={previousItem.to}>
+              <div className={direction}>Previous</div>
+              <div className={name}>{previousItem.name}</div>
+            </Link>
+          </Column>
+        )}
+        {nextItem.to && (
+          <Column className={link} colLg={6} colMd={4} colSm={2}>
+            <Link to={nextItem.to}>
+              <div className={direction}>Next</div>
+              <div className={name}>{nextItem.name}</div>
+            </Link>
+          </Column>
+        )}
+      </Row>
+    </Grid>
   </div>
 );
 
