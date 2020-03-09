fa0a84627ddd50b5e3bf44b38f9aa4287f8dee7e
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2020-03-09 15:55:41 -0500

    feat: add text fadeout

--- packages/gatsby-theme-carbon/src/components/FeedbackDialog/Comment.js
+++ packages/gatsby-theme-carbon/src/components/FeedbackDialog/Comment.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './Comment.module.scss';
 
-const Comment = () => (
-  <label className={styles.commentContainer} htmlFor="feedback-form-comment">
-    Comments (optional):
-    <textarea
-      rows={5}
-      name="feedback-form-comment"
-      id="feedback-form-comment"
-    />
-  </label>
-);
+const Comment = () => {
+  const handleBlur = e => {
+    e.target.scrollTop = 0;
+  };
+
+  return (
+    <label className={styles.commentContainer} htmlFor="feedback-form-comment">
+      Comments (optional):
+      <textarea
+        value="Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter crow’s nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard grog black jack gangway rutters."
+        onBlur={handleBlur}
+        rows={5}
+        name="feedback-form-comment"
+        id="feedback-form-comment"
+      />
+      <div className={styles.fadeout} />
+    </label>
+  );
+};
 
 export default Comment;
