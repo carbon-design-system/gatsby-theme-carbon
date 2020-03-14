fe18144f4d1294696a0dc446387d3f80c1a0ff2b
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2020-03-13 19:57:54 -0500

    fix: launch button active and hover styles

--- packages/gatsby-theme-carbon/src/components/FeedbackDialog/LaunchButton.js
+++ packages/gatsby-theme-carbon/src/components/FeedbackDialog/LaunchButton.js
@@ -1,22 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
+import cx from 'classnames';
 import styles from './LaunchButton.module.scss';
 
-function LaunchButton({ onClick, icon: Icon, filledIcon: FilledIcon }) {
-  const [hovering, setHovering] = useState(false);
+function LaunchButton({
+  onClick,
+  visible,
+  icon: Icon,
+  filledIcon: FilledIcon,
+}) {
+  const classNames = cx(styles.button, {
+    [styles.selected]: visible,
+  });
 
-  // TODO: hover && visible for both background change and inversion effects?
-  // action on submit?
   return (
     <button
-      onMouseEnter={() => setHovering(true)}
-      onMouseLeave={() => setHovering(false)}
       type="button"
       aria-haspopup
-      className={styles.button}
+      className={classNames}
       onClick={onClick}
       aria-label="This launches a modal form to give website feedback."
     >
-      {hovering ? <FilledIcon /> : <Icon />}
+      {visible ? <FilledIcon /> : <Icon />}
     </button>
   );
 }
