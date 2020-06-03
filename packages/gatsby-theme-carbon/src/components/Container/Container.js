888f698d0e9e33d37422f2bc0d37c3bae358edef
andrea.cardonaibm.com <andrea.cardonaibm.com>
2020-06-03 16:04:09 -0500

    feat: 791-header-shell-links-removing-left-nav - content left aligned with header nav

--- packages/gatsby-theme-carbon/src/components/Container/Container.js
+++ packages/gatsby-theme-carbon/src/components/Container/Container.js
@@ -4,6 +4,7 @@ import classnames from 'classnames';
 import cx from 'classnames';
 import NavContext from '../../util/context/NavContext';
 import useWindowSize from '../../util/hooks/useWindowSize';
+import useMetadata from '../../util/hooks/useMetadata';
 import { overlay, visible } from './Container.module.scss';
 
 const Container = ({ children, homepage, theme }) => {
@@ -12,6 +13,7 @@ const Container = ({ children, homepage, theme }) => {
   );
   const windowSize = useWindowSize();
   const lastWindowSize = useRef(windowSize);
+  const { navigationStyle } = useMetadata();
 
   const closeNavs = useCallback(() => {
     toggleNavState('leftNavIsOpen', 'close');
@@ -37,6 +39,7 @@ const Container = ({ children, homepage, theme }) => {
     container: true,
     'container--homepage': homepage,
     'container--dark': theme === 'dark',
+    'container--header--nav': navigationStyle,
   });
 
   return (
@@ -52,6 +55,7 @@ const Container = ({ children, homepage, theme }) => {
         id="main-content"
         aria-hidden={overlayVisible}
         className={containerClassNames}
+        useHeaderNavigation={navigationStyle}
       >
         {children}
       </main>
