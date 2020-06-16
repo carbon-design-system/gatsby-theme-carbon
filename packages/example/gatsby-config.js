205464549035ba546439767c017fe1a0d38265a9
Andrea N. Cardona <andreancardona@gmail.com>
2020-06-16 08:08:31 -0700

    pt1. updated back to default

--- packages/example/gatsby-config.js
+++ packages/example/gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
       resolve: 'gatsby-theme-carbon',
       options: {
         isSearchEnabled: true,
-        navigationStyle: 'header',
+        navigationStyle: '',
         mediumAccount: 'carbondesign',
         repository: {
           baseUrl:
