24149ae8d84c94e67efb0306ed2fe3b9c0f5a9f0
andrea.cardonaibm.com <andrea.cardonaibm.com>
2020-08-12 15:47:28 -0400

    LandmarkBeta: figuring out LandmarkButton

--- packages/gatsby-theme-carbon/src/components/LandmarksBeta/LandmarksBeta.js
+++ packages/gatsby-theme-carbon/src/components/LandmarksBeta/LandmarksBeta.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 import cx from 'classnames';
 import { landmarksWrapper, landmarksActive } from './LandmarksBeta.module.scss';
+import LandmarksButton from './LandmarksButton';
 
 const LandmarksBeta = ({ children }) => {
   const [active, setActive] = useState(false);
 
   return (
-    <div className={cx(landmarksWrapper, { landmarksActive: active })}>
-      {children}
-    </div>
+    <>
+      <div className={cx(landmarksWrapper, { landmarksActive: active })}>
+        {children}
+      </div>
+      <LandmarksButton />
+    </>
   );
 };
 
---
+++ packages/gatsby-theme-carbon/src/components/LandmarksBeta/LandmarksButton.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import cx from 'classnames';
+// import { AccessibilityAlt32 } from '@carbon/icons-react';
+import styles from '../FeedbackDialog/LaunchButton.module.scss';
+
+function LandmarksButton({ onClick, visible, icon: AccessibilityAlt32 }) {
+  const classNames = cx(styles.button, {
+    [styles.selected]: visible,
+  });
+
+  return (
+    <button
+      type="button"
+      aria-haspopup
+      className={classNames}
+      onClick={onClick}
+      aria-label="This launches a modal to activate or deactivate landmark highlighting">
+      {visible ? <AccessibilityAlt32 /> : <AccessibilityAlt32 />}
+    </button>
+  );
+}
+
+export default LandmarksButton;
