713ddb3eb1ba77db548db360cb1ea79cd397327d
andrea.cardonaibm.com <andrea.cardonaibm.com>
2020-08-12 15:16:14 -0400

    LandmarkBeta: looks good but need to add state

--- packages/gatsby-theme-carbon/src/components/LandmarksBeta/LandmarksBeta.js
+++ packages/gatsby-theme-carbon/src/components/LandmarksBeta/LandmarksBeta.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import cx from 'classnames';
+import { landmarksWrapper, landmarksActive } from './LandmarksBeta.module.scss';
 
 const LandmarksBeta = ({ children }) => {
-  const main = document.querySelectorAll('[role="main"]');
-  console.log(main);
+  const [active, setActive] = useState(false);
 
-  return <div className="landmark-wrapper">{children}</div>;
+  return (
+    <div className={cx(landmarksWrapper, { landmarksActive: active })}>
+      {children}
+    </div>
+  );
 };
 
 export default LandmarksBeta;
