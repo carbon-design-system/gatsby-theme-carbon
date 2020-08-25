fd31f64fe8a68a633763031aaa4ed8388c185182
Josefina Mancilla <josefinanoemi4@gmail.com>
2020-08-25 10:20:38 -0500

    fix: update checkmark animation

--- packages/gatsby-theme-carbon/src/components/FeedbackDialog/Checkmark.js
+++ packages/gatsby-theme-carbon/src/components/FeedbackDialog/Checkmark.js
@@ -2,15 +2,18 @@ import React from 'react';
 import styles from './Checkmark.module.scss';
 
 const Checkmark = () => (
-  <svg className={styles.svg} width="20" viewBox="0 0 28 28">
-    <circle r="14" cx="14" cy="14" fill="#fff" />
-    <polyline
-      className={styles.path}
-      points="7.8 13.75 12 17.95 20.2 9.8"
+  <svg className={styles.svg} width="20" viewBox="0 0 32 32">
+    <circle
+      cx="16"
+      cy="16"
+      r="12"
+      stroke="white"
       fill="white"
-      stroke="#0f62fe"
-      strokeMiterlimit="10"
-      strokeWidth="2.5"
+      strokeWidth="2"
+    />
+    <polygon
+      className="checkmark"
+      points="14 21.414 9 16.413 10.413 15 14 18.586 21.585 11 23 12.415 14 21.414"
     />
   </svg>
 );
