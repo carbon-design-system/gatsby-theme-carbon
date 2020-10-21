bb470794d177dfe54111911f2bf2ede1e7bd9272
Scott Strubberg <sstrubberg@pm.me>
2020-10-21 10:20:21 -0500

    style: knocking out leftnav importants

--- packages/gatsby-theme-carbon/src/components/PageHeader/PageHeader.js
+++ packages/gatsby-theme-carbon/src/components/PageHeader/PageHeader.js
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 import styles from './PageHeader.module.scss';
 
-const PageHeader = ({ title, tabs = [] }) => (
+const PageHeader = ({ title, theme, tabs = [] }) => (
   <div
     className={cx(styles.pageHeader, {
       [styles.withTabs]: tabs.length,
+      [styles.darkMode]: theme === 'dark',
     })}>
     <div className="bx--grid">
       <div className="bx--row">
