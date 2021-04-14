9814560f8803f71455a084c5032afed3ce49a0fe
Vince Picone <vp@vincepic.one>
2021-04-14 17:09:30 -0500

    chore: update gatsby plugins and remove emotion

--- packages/gatsby-theme-carbon/src/components/Homepage/Banner.js
+++ packages/gatsby-theme-carbon/src/components/Homepage/Banner.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import styled from '@emotion/styled';
 import { Grid, Row, Column } from '../Grid';
-
 import { column, row, grid } from './Banner.module.scss';
 
-const StyledGrid = styled(Grid)`
-  max-width: 100%;
-  background-image: url(${(props) => props.image});
-  background-position: ${(props) =>
-    props.position ? props.position : 'center'};
-`;
-
 const HomepageBanner = ({ image, position, renderText }) => (
-  <StyledGrid className={grid} position={position} image={image}>
+  <Grid
+    style={{
+      maxWidth: '100%',
+      backgroundImage: `url(${(props) => props.image})`,
+      backgroundPosition: position || 'center',
+    }}
+    className={grid}
+    position={position}
+    image={image}>
     <Row className={row}>
       <Column className={column}>{renderText()}</Column>
     </Row>
-  </StyledGrid>
+  </Grid>
 );
 
 export default HomepageBanner;
