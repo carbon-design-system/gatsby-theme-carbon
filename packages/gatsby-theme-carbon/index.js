41ed780c7937fcd9907d368f9aa8fb38d1c80e7b
Ale Davila <aledavila@ibm.com>
2021-12-13 14:52:25 -0600

    feat(expressive-list-container): adds expressive list container component

--- packages/gatsby-theme-carbon/index.js
+++ packages/gatsby-theme-carbon/index.js
@@ -16,5 +16,6 @@ export { default as ImageCard } from './src/components/ImageCard';
 export { default as SquareCard } from './src/components/SquareCard';
 export { Tabs, Tab } from './src/components/Tabs';
 export { default as InlineNotification } from './src/components/InlineNotification';
+export { default as ExpressiveListContainer } from './src/components/ExpressiveListContainer';
 // Homepage Template Components
 export { HomepageCallout, HomepageBanner } from './src/components/Homepage';
