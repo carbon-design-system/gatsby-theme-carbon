8bece9302c6851dc8f3a7d3ddc2fe78af3719efe
Scott Strubberg <sstrubberg@us.ibm.com>
2022-09-13 08:50:58 -0500

    feat(gtc): upgrade to v4 Gatsby, v11 Carbon, and associated docs (#1261)
    * feat(gtc): upgrade to v4 Gatsby, v11 Carbon, and associated docs
    
    * Update packages/example/src/pages/guides/v3-migration.mdx
    
    Co-authored-by: Taylor Jones <tay1orjones@users.noreply.github.com>
    
    * fix(icon): dropped the file in the right folder
    
    * docs(migration): add icons migration info
    
    * feat(gtc): split docs into two sets of migrations, updated deps round 1
    
    * feat(gtc): updated the rest of the gatsby deps
    
    * chore(gtc): updated carbon deps
    
    * fix(util): removed layout
    
    Co-authored-by: Taylor Jones <tay1orjones@users.noreply.github.com>
    Co-authored-by: Taylor Jones <taylor.jones826@gmail.com>

--- packages/gatsby-theme-carbon/src/util/theme.js
+++ packages/gatsby-theme-carbon/src/util/theme.js
@@ -1,11 +1,8 @@
-import { styles, white, spacing, layout } from '@carbon/elements';
+import { white } from '@carbon/elements';
 
 // Note, this is probably going to be removed in v2, we don't use it for anything
 const theme = {
-  typeStyles: styles,
   colors: { ...white },
-  spacing,
-  layout,
 };
 
 export default theme;
