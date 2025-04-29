a9caf04b5ab4c9c1864ef6ac57743299a65040dc
Alison Joseph <alisonejoseph@Gmail.com>
2025-04-29 09:14:43 -0500

    chore: remove pathprefix

--- packages/example/gatsby-config.js
+++ packages/example/gatsby-config.js
@@ -5,7 +5,6 @@ module.exports = {
     description: 'A Gatsby theme for the carbon design system',
     keywords: 'gatsby,theme,carbon',
   },
-  pathPrefix: `/gatsby-theme-carbon`,
   plugins: [
     {
       resolve: 'gatsby-plugin-manifest',
